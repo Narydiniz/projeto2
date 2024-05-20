d5f304f45bfa621babf764d483847f9c9dd9d36c
Narydiniz <senainaryara@gmail.com>
2024-05-20 10:44:49 -0300

    Responsividade

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//===============VARIAVEIS--GLOBAIS===================
 var suplementos = document.getElementById('suplemento')
 var alimento = document.getElementById('alimentos')
 var belezas = document.querySelector('div#beleza')
@@ -7,37 +8,99 @@ var carrinho = document.getElementById('cart')
 var compras = document.getElementById('compras')
 var infoCart = document.getElementsByClassName('main-cart-container-sumir')[0]
 var nav = document.getElementsByClassName('navegacao')[0]
-function clicars(){
+
+
+//============VARIAVEIS DOS BOTÕES=================
+var buttonT = document.getElementById('button-products-navT')
+var buttonS = document.getElementById('button-products-navS')
+var buttonA = document.getElementById('button-products-navA')
+var buttonR = document.getElementById('button-products-navR')
+var buttonB = document.getElementById('button-products-navB')
+
+
+//=======FUNÇÕES SUMIR/APARECER PRODUTOS E BOTÕES======
+
+
+//=====FUNÇÃO -> BOTÃO DE SUPLEMENTO=====
+function clicarS(){
     suplementos.style.display = 'block'
     relaxamentos.style.display = 'none'
     belezas.style.display = 'none'
     alimento.style.display = 'none'
     infoCart.style.display = 'none'
+    buttonS.style.background = '#2c3a0f'
+    buttonS.style.color = 'white'
+    buttonT.style.color = 'black'
+    buttonB.style.color = 'black'
+    buttonR.style.color = 'black'
+    buttonA.style.color = 'black'
+    buttonT.style.background = 'white'
+    buttonB.style.background = 'white'
+    buttonR.style.background = 'white'
+    buttonA.style.background = 'white'
 }
-function clicara(){
+
+//=====FUNÇÃO -> BOTÃO DE ALIMENTOS=====
+function clicarA(){
     suplementos.style.display = 'none'
     relaxamentos.style.display = 'none'
     belezas.style.display = 'none'
     alimento.style.display = 'block'
     carrinho.style.display = 'block'
     infoCart.style.display = 'none'
+    buttonA.style.background = '#2c3a0f'
+    buttonA.style.color = 'white'
+    buttonT.style.color = 'black'
+    buttonB.style.color = 'black'
+    buttonR.style.color = 'black'
+    buttonS.style.color = 'black'
+    buttonT.style.background = 'white'
+    buttonB.style.background = 'white'
+    buttonR.style.background = 'white'
+    buttonS.style.background = 'white'
 }
-function clicarr(){
+
+//=====FUNÇÃO -> BOTÃO DE RELAXAMENTO=====
+function clicarR(){
     suplementos.style.display = 'none'
     relaxamentos.style.display = 'block'
     belezas.style.display = 'none'
     alimento.style.display = 'none'
     carrinho.style.display = 'block'
     infoCart.style.display = 'none'
+    buttonR.style.background = '#2c3a0f'
+    buttonR.style.color = 'white'
+    buttonT.style.color = 'black'
+    buttonB.style.color = 'black'
+    buttonA.style.color = 'black'
+    buttonS.style.color = 'black'
+    buttonT.style.background = 'white'
+    buttonB.style.background = 'white'
+    buttonA.style.background = 'white'
+    buttonS.style.background = 'white'
 }
-function clicarb(){
+
+//=====FUNÇÃO -> BOTÃO DE BELEZA=====
+function clicarB(){
     suplementos.style.display = 'none'
     relaxamentos.style.display = 'none'
     belezas.style.display = 'block'
     alimento.style.display = 'none'
     carrinho.style.display = 'block'
     infoCart.style.display = 'none'
+    buttonB.style.background = '#2c3a0f'
+    buttonB.style.color = 'white'
+    buttonT.style.color = 'black'
+    buttonR.style.color = 'black'
+    buttonA.style.color = 'black'
+    buttonS.style.color = 'black'
+    buttonT.style.background = 'white'
+    buttonR.style.background = 'white'
+    buttonA.style.background = 'white'
+    buttonS.style.background = 'white'
 }
+
+//=====FUNÇÃO -> BOTÃO DE IR AO CARRINHO=====
 function cart(){
     suplementos.style.display = 'none'
     relaxamentos.style.display = 'none'
@@ -47,6 +110,8 @@ function cart(){
     infoCart.style.display = 'block'
     nav.style.display = 'none'
 }
+
+//=====FUNÇÃO -> BOTÃO DE IR AS COMPRAS=====
 function ircompras(){
     suplementos.style.display = 'block'
     relaxamentos.style.display = 'block'
@@ -56,6 +121,29 @@ function ircompras(){
     infoCart.style.display = 'none'
     nav.style.display = 'block'
 }
+
+//=====FUNÇÃO -> BOTÃO DE TODOS OS PRODUTOS=====
+function clicarT(){
+    suplementos.style.display = 'block'
+    relaxamentos.style.display = 'block'
+    belezas.style.display = 'block'
+    alimento.style.display = 'block'
+    carrinho.style.display = 'block'
+    infoCart.style.display = 'none'
+    nav.style.display = 'block'
+    buttonT.style.background = '#2c3a0f'
+    buttonT.style.color = 'white'
+    buttonS.style.color = 'black'
+    buttonB.style.color = 'black'
+    buttonR.style.color = 'black'
+    buttonA.style.color = 'black'
+    buttonS.style.background = 'white'
+    buttonB.style.background = 'white'
+    buttonR.style.background = 'white'
+    buttonA.style.background = 'white'
+}
+
+
 let count = 1;
 document.getElementById("radio1").checked = true;
 
@@ -73,13 +161,15 @@ function nextImage(){
 const btnShowSweetAlert = document.querySelector('#btn-show-sweetalert');
 btnShowSweetAlert.addEventListener('click',function(){
     Swal.fire({
-        title: "Sweet!",
-        text: "Modal with a custom image.",
+        title: "Boas-Vindas a nossa loja virtual!",
+        text: "Você estava louco(a) por uma loja de produtos naturais com uma variedade de produtos para uma vida mais saudável? Pois essa loja chegou! Venha para a Empório Natural e conheça todas as nossas novidades!",
         imageUrl: "imagens/Alert.png",
         webkitURL:"produtos.html",
         imageWidth: 400,
         imageHeight: 200,
-        footer: '<a class="ancora" href="produtos.html">Visite nossos produtos</a>',
+        showCloseButton: true,
+        allowEscapeKey: true,
+        confirmButtonText: '<a class="ancora" href="produtos.html">Confira nossos produtos</a>',
         imageAlt: "Custom image"
         
       });
